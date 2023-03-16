168c546dbcabfbe654c7a630dc04c40a56ba3b62
Foxive <Foxlieox@gmail.com>
2023-03-17 04:05:09 +0700

    Add HW1 Terminal.md and .sh script

---
+++ hw1_script.sh
@@ -0,0 +1,12 @@
+mkdir qa_homework
+cd qa_homework
+mkdir hw1 hw2 hw3
+cd hw1
+touch 1.txt 2.txt 3.txt 1.json 2.json
+mkdir a b c
+ls a
+cd hw1
+mv *.json a
+
+echo "Скрипт выполнен. Пункты 3, 4, 5, 6, 7, 8, 13 завершены"
+exit 0
